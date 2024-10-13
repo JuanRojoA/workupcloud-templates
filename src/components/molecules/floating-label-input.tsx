234054b3c9c19e4ad586adfacd08dc865b54195a
Juan Rojo <juan.rojo@novadeha.com>
2024-10-12 20:22:02 -0500

    refactor(components): update FloatingLabelInput component to integrate with React Hook Form

--- src/components/molecules/floating-label-input.tsx
+++ src/components/molecules/floating-label-input.tsx
@@ -1,6 +1,7 @@
-import React, { useState, forwardRef, useId } from "react";
+import React, { useState, useId } from "react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
 import type { FieldError } from "react-hook-form";
 
 type InputType =
@@ -16,33 +17,34 @@ type InputType =
  * Props for the FloatingLabelInput component.
  */
 interface FloatingLabelInputProps
-	extends Omit<
-		React.InputHTMLAttributes<HTMLInputElement>,
-		"type" | "onChange"
-	> {
+	extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {
 	type?: InputType;
 	label: string;
 	name: string;
 	wrapperClassName?: string;
+	className?: string;
 	error?: FieldError;
-	value?: string;
-	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // Ensure onChange is typed here
+	id?: string;
 }
 
 /**
- * An input component with a floating label, designed to be used as a controlled component.
+ * An input component with a floating label, integrated with React Hook Form.
  *
  * The `FloatingLabelInput` component provides an input field with a label that animates to a floating position
  * when the input is focused or contains a value. It integrates seamlessly with React Hook Form and Zod for form state management
- * and validation using the `Controller` component.
+ * and validation, and is designed to work well even when inputs are distributed across different parts of the UI.
  *
- * __Note:__ Do not use this component for date inputs. Use the `FloatingLabelDateInput` component instead.
- * Nor for color inputs; use the `FloatingLabelColorInput` component.
+ * ### Features:
+ * - **Floating Label**: The label smoothly transitions between its initial position and a floating position.
+ * - **Form Integration**: Uses `register` from React Hook Form to integrate with form management and validation.
+ * - **Accessible**: Properly associates the label with the input for screen readers and assistive technologies.
+ * - **Error Handling**: Displays validation errors and associates them with the input for accessibility.
+ * - **Flexible Styling**: Accepts additional class names for both the wrapper and the input for easy customization.
  *
- * ___Usage Examples:___
+ * ### Usage Examples:
  *
  * ```jsx
- * import { useForm, Controller } from "react-hook-form";
+ * import { useForm } from "react-hook-form";
  * import { z } from "zod";
  * import { zodResolver } from "@hookform/resolvers/zod";
  * import { FloatingLabelInput } from "@/components/FloatingLabelInput";
@@ -52,7 +54,7 @@ interface FloatingLabelInputProps
  * });
  *
  * const Example = () => {
- *   const { control, handleSubmit, formState: { errors } } = useForm({
+ *   const { register, handleSubmit, formState: { errors } } = useForm({
  *     resolver: zodResolver(FormSchema),
  *   });
  *
@@ -62,19 +64,12 @@ interface FloatingLabelInputProps
  *
  *   return (
  *     <form onSubmit={handleSubmit(onSubmit)}>
- *       <Controller
- *         control={control}
+ *       <FloatingLabelInput
+ *         label="Email"
+ *         type="email"
  *         name="email"
- *         render={({ field, fieldState }) => (
- *           <FloatingLabelInput
- *             label="Email"
- *             type="email"
- *             error={fieldState.error}
- *             value={field.value}
- *             onChange={field.onChange}
- *             wrapperClassName="mb-4"
- *           />
- *         )}
+ *         error={errors.email}
+ *         wrapperClassName="mb-4"
  *       />
  *       <button type="submit">Submit</button>
  *     </form>
@@ -90,94 +85,85 @@ interface FloatingLabelInputProps
  * @param {FieldError} [props.error] - The error object from React Hook Form.
  * @param {string} [props.wrapperClassName] - Additional class names for the input wrapper.
  * @param {string} [props.className] - Additional class names for the input element.
- * @param {string} [props.value] - The current value of the input field.
- * @param {Function} [props.onChange] - Callback function when the input value changes.
  * @param {string} [props.id] - The id of the input element.
  *
  * @returns {JSX.Element} The rendered FloatingLabelInput component.
  */
 export const FloatingLabelInput = React.memo(
-	forwardRef<HTMLInputElement, FloatingLabelInputProps>(
-		(
-			{
-				type = "text",
-				label,
-				name,
-				wrapperClassName,
-				className,
-				id,
-				error,
-				value = "",
-				onChange,
-				...rest
-			},
-			ref,
-		) => {
-			const uniqueId = useId();
-			const inputId = id || uniqueId;
+	({
+		type = "text",
+		label,
+		name,
+		wrapperClassName,
+		className,
+		id,
+		error,
+		...rest
+	}: FloatingLabelInputProps) => {
+		const uniqueId = useId();
+		const inputId = id || uniqueId;
 
-			const [isFocused, setIsFocused] = useState(false);
+		const { register, watch } = useFormContext();
 
-			const hasValue = value !== "";
+		const [isFocused, setIsFocused] = useState(false);
 
-			return (
-				<div className={cn("relative", wrapperClassName)}>
-					<div className="relative w-full h-max bg-transparent">
-						<Input
-							id={inputId}
-							type={type}
-							name={name}
-							className={cn(
-								"peer placeholder-transparent placeholder-shown:placeholder-transparent",
-								{ "border-primary": isFocused },
-								{ "border-red-500": error },
-								className,
-							)}
-							aria-invalid={error ? "true" : "false"}
-							aria-describedby={error ? `${inputId}-error` : undefined}
-							ref={ref}
-							onFocus={(e) => {
-								setIsFocused(true);
-								rest.onFocus?.(e);
-							}}
-							onBlur={(e) => {
-								setIsFocused(false);
-								rest.onBlur?.(e);
-							}}
-							value={value}
-							onChange={(e) => {
-								onChange?.(e);
-							}}
-							{...rest}
-						/>
-						<label
-							htmlFor={inputId}
-							className={cn(
-								"absolute left-3 top-[47%] transform -translate-y-1/2 transition-all duration-200 ease-in-out text-zinc-600 pointer-events-none bg-white px-1 leading-[0]",
-								{
-									"text-xs -top-0.5 left-2 font-semibold bg-zinc-100 rounded-md px-1":
-										hasValue || isFocused,
-									"text-sm": !hasValue && !isFocused,
-									"text-red-500": error,
-								},
-							)}
-						>
-							{label}
-						</label>
-					</div>
-					{error && (
-						<p
-							id={`${inputId}-error`}
-							className="mt-1 text-sm text-red-500"
-							role="alert"
-						>
-							{error.message}
-						</p>
-					)}
+		const value = watch(name) || "";
+
+		const hasValue = value !== "";
+
+		return (
+			<div className={cn("relative", wrapperClassName)}>
+				<div className="relative w-full h-max bg-transparent">
+					<Input
+						{...register(name)}
+						id={inputId}
+						type={type}
+						name={name}
+						className={cn(
+							"peer placeholder-transparent placeholder-shown:placeholder-transparent",
+							{ "border-primary": isFocused },
+							{ "border-red-500": error },
+							className,
+						)}
+						aria-invalid={error ? "true" : "false"}
+						aria-describedby={error ? `${inputId}-error` : undefined}
+						onFocus={(e) => {
+							setIsFocused(true);
+							rest.onFocus?.(e);
+						}}
+						onBlur={(e) => {
+							setIsFocused(false);
+							rest.onBlur?.(e);
+						}}
+						{...rest}
+					/>
+					<label
+						htmlFor={inputId}
+						className={cn(
+							"absolute left-3 top-[47%] transform -translate-y-1/2 transition-all duration-200 ease-in-out text-zinc-600 pointer-events-none bg-white px-1 leading-[0]",
+							{
+								"text-xs -top-0.5 left-2 font-semibold bg-zinc-100 rounded-md px-1":
+									hasValue || isFocused,
+								"text-sm": !hasValue && !isFocused,
+								"text-red-500": error,
+							},
+						)}
+					>
+						{label}
+					</label>
 				</div>
-			);
-		},
-	),
+				{error && (
+					<p
+						id={`${inputId}-error`}
+						className="mt-1 text-sm text-red-500"
+						role="alert"
+					>
+						{error.message}
+					</p>
+				)}
+			</div>
+		);
+	},
 );
 
 FloatingLabelInput.displayName = "FloatingLabelInput";
