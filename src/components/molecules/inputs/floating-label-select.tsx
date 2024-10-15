83a1fc3d0b1390c75e74d5e914c4a784d5c956b2
Juan Rojo <juan.rojo@novadeha.com>
2024-10-15 12:28:30 -0500

    Refactor(FloatingLabelSelect): Update FloatingLabelSelect component to use react-hook-form v7 and improve accessibility

--- src/components/molecules/inputs/floating-label-select.tsx
+++ src/components/molecules/inputs/floating-label-select.tsx
@@ -1,5 +1,3 @@
-import { useState, useId } from "react";
-import { cn } from "@/lib/utils.ts";
 import { Button } from "@/components/ui/button.tsx";
 import {
 	Command,
@@ -14,10 +12,15 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover.tsx";
+import { cn } from "@/lib/utils.ts";
 import { Check, ChevronsUpDown } from "lucide-react";
-import { useFormContext } from "react-hook-form";
+import React, { useId, useState } from "react";
+import { Controller, useFormContext } from "react-hook-form";
 import type { FieldError } from "react-hook-form";
 
+/**
+ * Props for the FloatingLabelSelect component.
+ */
 interface FloatingLabelSelectProps {
 	label: string;
 	name: string;
@@ -26,56 +29,15 @@ interface FloatingLabelSelectProps {
 	className?: string;
 	error?: FieldError;
 	id?: string;
+	onBlur?: (event: React.FocusEvent<HTMLButtonElement | HTMLElement>) => void;
 }
 
 /**
- * A select component with a floating label, integrated with React Hook Form.
+ * A React component for a select input with a floating label.
  *
- * The `FloatingLabelSelect` component provides a select field with a label that animates to a floating position
- * when the select is focused or contains a value. It uses a hidden input to integrate with React Hook Form and Zod,
- * allowing for seamless form state management and validation.
- *
- * ### Usage Examples:
- *
- * ```jsx
- * import { useForm } from "react-hook-form";
- * import { z } from "zod";
- * import { zodResolver } from "@hookform/resolvers/zod";
- * import { FloatingLabelSelect } from "@/components/FloatingLabelSelect";
- *
- * const languages = [
- *   { label: "English", value: "en" },
- *   { label: "French", value: "fr" },
- *   // ...more options
- * ];
- *
- * const FormSchema = z.object({
- *   language: z.string().nonempty({ message: "Please select a language." }),
- * });
- *
- * const Example = () => {
- *   const { register, handleSubmit, formState: { errors } } = useForm({
- *     resolver: zodResolver(FormSchema),
- *   });
- *
- *   const onSubmit = (data) => {
- *     // Handle form submission
- *   };
- *
- *   return (
- *     <form onSubmit={handleSubmit(onSubmit)}>
- *       <FloatingLabelSelect
- *         label="Language"
- *         name="language"
- *         options={languages}
- *         error={errors.language}
- *         wrapperClassName="mb-4"
- *       />
- *       <button type="submit">Submit</button>
- *     </form>
- *   );
- * };
- * ```
+ * This component provides a user-friendly select input with a floating label that animates
+ * when the input is focused or has a value.
+ * It uses `react-hook-form` for form integration and a custom command menu for the select options.
  *
  * @component
  * @param {FloatingLabelSelectProps} props - Props for configuring the FloatingLabelSelect component.
@@ -86,10 +48,31 @@ interface FloatingLabelSelectProps {
  * @param {string} [props.wrapperClassName] - Additional class names for the select wrapper.
  * @param {string} [props.className] - Additional class names for the select component.
  * @param {string} [props.id] - The id of the select component.
+ * @returns {React.ReactElement} The rendered FloatingLabelSelect component.
  *
- * @returns {JSX.Element} The rendered FloatingLabelSelect component.
+ * @example
+ * // Basic usage
+ * <FloatingLabelSelect
+ *   label="Choose an option"
+ *   name="option"
+ *   options={[
+ *     { label: "Option 1", value: "option1" },
+ *     { label: "Option 2", value: "option2" },
+ *   ]}
+ * />
+ *
+ * @example
+ * // With custom class names and error message
+ * <FloatingLabelSelect
+ *   label="Select a category"
+ *   name="category"
+ *   options={[ ... ]}
+ *   wrapperClassName="mb-4"
+ *   className="border-2 border-blue-500"
+ *   error={{ message: "Please select a category" }}
+ * />
  */
-export const FloatingLabelSelect = ({
+export const FloatingLabelSelect: React.FC<FloatingLabelSelectProps> = ({
 	label,
 	name,
 	options,
@@ -98,108 +81,106 @@ export const FloatingLabelSelect = ({
 	id,
 	error,
 	...rest
-}: FloatingLabelSelectProps): JSX.Element => {
+}: FloatingLabelSelectProps): React.ReactElement => {
 	const uniqueId = useId();
 	const inputId = id || uniqueId;
 
-	const { register, setValue, watch } = useFormContext();
+	const { control } = useFormContext();
 
 	const [open, setOpen] = useState(false);
 	const [isFocused, setIsFocused] = useState(false);
 
-	const selectedValue = watch(name) || "";
-
-	const handleChange = (value: string) => {
-		setValue(name, value, { shouldValidate: true });
-	};
-
-	const hasValue = selectedValue !== "";
-
 	return (
 		<div className={cn("relative", wrapperClassName)}>
-			<div className="relative w-full h-max bg-transparent">
-				<input
-					{...register(name)}
-					type="hidden"
-					id={inputId}
-					name={name}
-					value={selectedValue}
-				/>
-				<Popover open={open} onOpenChange={setOpen}>
-					<PopoverTrigger asChild>
-						<Button
-							variant="outline"
-							// biome-ignore lint/a11y/useSemanticElements: <explanation>
-							role="combobox"
-							aria-expanded={open}
-							className={cn(
-								"w-full justify-between font-normal peer text-left hover:bg-white",
-								!hasValue && "text-muted-foreground",
-								{ "border-primary": isFocused },
-								{ "border-red-500": error },
-								className,
-							)}
-							onFocus={() => setIsFocused(true)}
-							onBlur={() => setIsFocused(false)}
-							{...rest}
-						>
-							{selectedValue ? (
-								options.find((option) => option.value === selectedValue)?.label
-							) : (
-								<p />
-							)}
-							<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-						</Button>
-					</PopoverTrigger>
-					<PopoverContent className="w-[--radix-popover-trigger-width] max-h-[--radix-popover-content-available-height] p-2">
-						<Command className="w-full">
-							<CommandInput placeholder={`Search ${label}...`} />
-							<CommandList>
-								<CommandEmpty>No {label} found.</CommandEmpty>
-								<CommandGroup>
-									{options.map((option) => (
-										<CommandItem
-											keywords={[option.label]}
-											key={option.value}
-											value={option.value}
-											onSelect={(currentValue) => {
-												handleChange(
-													currentValue === selectedValue ? "" : currentValue,
-												);
-												setOpen(false);
-											}}
-										>
-											<Check
-												className={cn(
-													"mr-2 h-4 w-4",
-													selectedValue === option.value
-														? "opacity-100"
-														: "opacity-0",
-												)}
-											/>
-											{option.label}
-										</CommandItem>
-									))}
-								</CommandGroup>
-							</CommandList>
-						</Command>
-					</PopoverContent>
-				</Popover>
-				<label
-					htmlFor={inputId}
-					className={cn(
-						"absolute left-3 top-[50%] transform -translate-y-1/2 transition-all duration-200 ease-in-out text-zinc-600 pointer-events-none bg-white px-1 leading-[0]",
-						{
-							"text-xs -top-0.5 left-2 font-semibold bg-zinc-100 rounded-md px-1":
-								hasValue || isFocused || open,
-							"text-sm": !hasValue && !isFocused && !open,
-							"text-red-500": error,
-						},
-					)}
-				>
-					{label}
-				</label>
-			</div>
+			<Controller
+				name={name}
+				control={control}
+				render={({ field: { value, onChange, onBlur } }) => {
+					const selectedOption = options.find(
+						(option) => option.value === value,
+					);
+					const hasValue = !!selectedOption;
+
+					return (
+						<>
+							<Popover open={open} onOpenChange={setOpen}>
+								<PopoverTrigger asChild>
+									<Button
+										variant="outline"
+										role="combobox"
+										aria-expanded={open}
+										className={cn(
+											"w-full justify-between font-normal peer text-left hover:bg-white",
+											!hasValue && "text-muted-foreground",
+											{ "border-primary": isFocused },
+											{ "border-red-500": error },
+											className,
+										)}
+										onFocus={() => setIsFocused(true)}
+										onBlur={(_e) => {
+											onBlur();
+										}}
+										{...rest}
+									>
+										{hasValue ? (
+											<div className="flex items-center">
+												{selectedOption?.label}
+											</div>
+										) : (
+											<span className="text-muted-foreground">
+												Select an option
+											</span>
+										)}
+										<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent className="w-[--radix-popover-trigger-width] max-h-[--radix-popover-content-available-height] p-2">
+									<Command className="w-full">
+										<CommandInput placeholder={`Search ${label}...`} />
+										<CommandList>
+											<CommandEmpty>No {label} found.</CommandEmpty>
+											<CommandGroup>
+												{options.map((option) => (
+													<CommandItem
+														keywords={[option.label]}
+														key={option.value}
+														value={option.value}
+														onSelect={() => {
+															option.value !== value
+																? onChange(option.value)
+																: onChange(null);
+														}}
+														className="flex items-center justify-between"
+													>
+														{option.label}
+														{value === option.value && (
+															<Check className="h-4 w-4" />
+														)}
+													</CommandItem>
+												))}
+											</CommandGroup>
+										</CommandList>
+									</Command>
+								</PopoverContent>
+							</Popover>
+							<label
+								htmlFor={inputId}
+								className={cn(
+									"absolute left-3 top-[50%] transform -translate-y-1/2 transition-all duration-200 ease-in-out text-zinc-600 pointer-events-none bg-white px-1",
+									{
+										"text-xs -top-0.5 left-2 font-semibold bg-zinc-100 rounded-md px-1":
+											hasValue || isFocused || open,
+										"text-sm": !hasValue && !isFocused && !open,
+										"text-red-500": error,
+									},
+								)}
+							>
+								{label}
+							</label>
+						</>
+					);
+				}}
+			/>
 			{error && (
 				<p
 					id={`${inputId}-error`}
