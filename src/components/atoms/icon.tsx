7d9d7a5a049700d5b20e75c2dd463fa07f3e9ac2
Juan Rojo <juan.rojo@novadeha.com>
2024-10-17 17:02:10 -0500

    feat: add a dynamic icon component with error handling
    Introduced an Icon component that dynamically imports icons from the @phosphor-icons/react library based on the provided iconName. The component includes error handling for missing icons and displays a loading spinner while the icon is being fetched. This enhancement improves flexibility and user experience when using icons.

---
+++ src/components/atoms/icon.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { IconProps } from "@phosphor-icons/react";
+
+interface IIconProps {
+    iconName: string;
+    propsIcon?: IconProps;
+}
+
+const Icon = ({ iconName, propsIcon = {} }: IIconProps) => {
+    const [IconComponent, setIconComponent] = useState<React.ElementType | null>(null);
+    const [hasError, setHasError] = useState(false);
+
+    useEffect(() => {
+        const cleanedIconName = iconName.trim();
+
+        const importIcon = async () => {
+            try {
+                const module:any = await import(`@phosphor-icons/react`);
+                const LoadedIcon = module[cleanedIconName];
+                if (LoadedIcon) {
+                    setIconComponent(() => LoadedIcon);
+                } else {
+                    setHasError(true);
+                }
+            } catch (error) {
+                setHasError(true);
+            }
+        };
+
+        importIcon();
+    }, [iconName]);
+
+    if (hasError) {
+        return <span>Icon not found</span>;
+    }
+
+    if (!IconComponent) {
+        return (
+            <div className="flex items-center justify-center w-6 h-6">
+                <div className="w-4 h-4 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+            </div>
+        );
+    }
+
+    return <IconComponent {...propsIcon} />;
+};
+
+export default Icon;
+export type { IIconProps };
